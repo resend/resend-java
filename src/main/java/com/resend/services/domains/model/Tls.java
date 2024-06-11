a56bf598625d88897a24cae6f3ff07d23f180951
kewyn <kewyn.silva@virtus.ufcg.edu.br>
2024-06-11 11:30:09 -0300

    fix: Add docs for TLS config

--- src/main/java/com/resend/services/domains/model/Tls.java
+++ src/main/java/com/resend/services/domains/model/Tls.java
@@ -1,19 +1,46 @@
 package com.resend.services.domains.model;
 
+/**
+ * Enum representing the TLS settings for a domain.
+ */
 public enum Tls {
+    /**
+     *  Represents an enforced TLS setting.
+     */
     ENFORCED("enforced"),
+    /**
+     *  Represents an opportunistic TLS setting.
+     */
     OPPORTUNISTIC("opportunistic");
 
+    /**
+     * Holds the string representation of the enum value.
+     */
     private final String value;
 
+    /**
+     * Constructor for the Tls enum.
+     *
+     * @param value The string representation of the TLS setting.
+     */
     Tls(String value) {
         this.value = value;
     }
 
+    /**
+     * Retrieves the string representation of the TLS setting.
+     *
+     * @return The string representation of the TLS setting.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Returns the string representation of the TLS setting.
+     *
+     * @return The string representation of the TLS setting.
+     */
     @Override
     public String toString() {
         return this.value;

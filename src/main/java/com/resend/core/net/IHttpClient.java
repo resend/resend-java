4a451a58f74e10886526b13ca0ae313ecf7ef094
Kewyn Akshlley <kewynakshlley@gmail.com>
2025-04-30 09:33:27 -0300

    feat: Add idempotent key (#44)

--- src/main/java/com/resend/core/net/IHttpClient.java
+++ src/main/java/com/resend/core/net/IHttpClient.java
@@ -3,6 +3,8 @@ package com.resend.core.net;
 import com.resend.core.exception.ResendException;
 import okhttp3.MediaType;
 
+import java.util.Map;
+
 /**
  * An interface representing an HTTP client for performing HTTP requests and receiving responses.
  *
@@ -21,4 +23,17 @@ public interface IHttpClient<T> {
      * @return An {@link AbstractHttpResponse} representing the response from the server.
      */
     AbstractHttpResponse<T> perform(final String path, final String apiKey, final HttpMethod method, final String payload, final MediaType mediaType);
+
+    /**
+     * Perform an HTTP request with the specified path, method, and payload.
+     *
+     * @param path    The path or endpoint of the request.
+     * @param apiKey  The API Key used to authenticate the request.
+     * @param method  The HTTP method (GET, POST, PUT, DELETE, etc.).
+     * @param payload The payload or data to send with the request.
+     * @param mediaType The media type of the request.
+     * @param additionalHeaders A map of header-name → header-value to add.
+     * @return An {@link AbstractHttpResponse} representing the response from the server.
+     */
+    AbstractHttpResponse<T> perform(final String path, final String apiKey, final HttpMethod method, final String payload, final MediaType mediaType, final Map<String,String> additionalHeaders);
 }

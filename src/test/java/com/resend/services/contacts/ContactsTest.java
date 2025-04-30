671913a4b1e47b17093dfbeaa3d8a6b223a3026f
Kewyn Akshlley <kewynakshlley@gmail.com>
2025-04-30 09:44:33 -0300

    feat: Add get contact by e-mail (#45)

--- src/test/java/com/resend/services/contacts/ContactsTest.java
+++ src/test/java/com/resend/services/contacts/ContactsTest.java
@@ -84,4 +84,38 @@ public class ContactsTest {
         assertEquals(expectedResponse.getId(), res.getId());
         assertEquals(expectedResponse.getObject(), res.getObject());
     }
+
+    @Test
+    public void testGetContactById_Success() throws ResendException {
+        GetContactOptions params = GetContactOptions.builder()
+                .id("e169aa45-1ecf-4183-9955-b1499d5701d3")
+                .audienceId("78261eea-8f8b-4381-83c6-79fa7120f1cf")
+                .build();
+        GetContactResponseSuccess expected = ContactsUtil.getContactResponseSuccess();
+
+        when(contacts.get(params)).thenReturn(expected);
+
+        GetContactResponseSuccess res = contacts.get(params);
+
+        assertNotNull(res);
+        assertEquals(expected, res);
+        verify(contacts, times(1)).get(params);
+    }
+
+    @Test
+    public void testGetContactByEmail_Success() throws ResendException {
+        GetContactOptions params = GetContactOptions.builder()
+                .email("user@example.com")
+                .audienceId("78261eea-8f8b-4381-83c6-79fa7120f1cf")
+                .build();
+        GetContactResponseSuccess expected = ContactsUtil.getContactResponseSuccess();
+
+        when(contacts.get(params)).thenReturn(expected);
+
+        GetContactResponseSuccess res = contacts.get(params);
+
+        assertNotNull(res);
+        assertEquals(expected, res);
+        verify(contacts, times(1)).get(params);
+    }
 }
